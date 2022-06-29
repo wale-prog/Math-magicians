e26c25ebca47e4c35d02751ce0d1bd74cacfa632
Olawale <olapetanwale@outlook.com>
2022-06-29 21:48:16 +0100

    Fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,5 @@
-/* eslint-disable */
 import { Component } from 'react';
 import calculate from './calculate';
-
 
 class Calculator extends Component {
   constructor(props) {
@@ -9,17 +7,17 @@ class Calculator extends Component {
     this.state = {
       total: '',
       next: '',
-      operation: ''
+      operation: '',
     };
 
     this.handleClick = this.handleClick.bind(this);
-  }    
-  
+  }
+
   handleClick = (event) => {
     const btnValue = event.target.value;
-    this.setState((prevState) =>({
+    this.setState((prevState) => ({
       ...calculate(prevState, btnValue),
-    }))
+    }));
   }
 
   render() {
@@ -27,28 +25,32 @@ class Calculator extends Component {
     return (
       <div className="main-container">
         <div className="input">
-          <span> {next || operation || total || '0'} </span>
+          <span>
+            {' '}
+            {next || operation || total || '0'}
+            {' '}
+          </span>
         </div>
         <form className="calc-btns">
           <input type="button" value="AC" onClick={this.handleClick} />
-          <input type="button" value="+/-" onClick={this.handleClick}  />
-          <input type="button" value="%" onClick={this.handleClick}  />
-          <input type="button" value='÷' className="peach" onClick={this.handleClick}  />
-          <input type="button" value="7" onClick={this.handleClick}  />
-          <input type="button" value="8" onClick={this.handleClick}  />
-          <input type="button" value="9" onClick={this.handleClick}  />
-          <input type="button" value='x' className="peach" onClick={this.handleClick}  />
-          <input type="button" value="4" onClick={this.handleClick}  />
-          <input type="button" value="5" onClick={this.handleClick}  />
-          <input type="button" value="6" onClick={this.handleClick}  />
-          <input type="button" value='-' className="peach" onClick={this.handleClick}  />
-          <input type="button" value="3" onClick={this.handleClick}  />
-          <input type="button" value="2" onClick={this.handleClick}  />
-          <input type="button" value="1" onClick={this.handleClick}  />
-          <input type="button" value="+" className="peach" onClick={this.handleClick}  />
-          <input type="button" value="0" className="zero" onClick={this.handleClick}  />
-          <input type="button" value="." onClick={this.handleClick}  />
-          <input type="button" value= '=' className="peach" onClick={this.handleClick}  />
+          <input type="button" value="+/-" onClick={this.handleClick} />
+          <input type="button" value="%" onClick={this.handleClick} />
+          <input type="button" value="÷" className="peach" onClick={this.handleClick} />
+          <input type="button" value="7" onClick={this.handleClick} />
+          <input type="button" value="8" onClick={this.handleClick} />
+          <input type="button" value="9" onClick={this.handleClick} />
+          <input type="button" value="x" className="peach" onClick={this.handleClick} />
+          <input type="button" value="4" onClick={this.handleClick} />
+          <input type="button" value="5" onClick={this.handleClick} />
+          <input type="button" value="6" onClick={this.handleClick} />
+          <input type="button" value="-" className="peach" onClick={this.handleClick} />
+          <input type="button" value="3" onClick={this.handleClick} />
+          <input type="button" value="2" onClick={this.handleClick} />
+          <input type="button" value="1" onClick={this.handleClick} />
+          <input type="button" value="+" className="peach" onClick={this.handleClick} />
+          <input type="button" value="0" className="zero" onClick={this.handleClick} />
+          <input type="button" value="." onClick={this.handleClick} />
+          <input type="button" value="=" className="peach" onClick={this.handleClick} />
         </form>
       </div>
     );
